2fece05d1e10f1f64224643c1fdd1d50696a327d
Paul Thomson <paulthomson@google.com>
2020-02-29 15:54:06 +0000

    Safer retry

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"os"
-	"path"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -37,6 +36,7 @@ type Asset struct {
 	Name  string `json:"name"`
 	Label string `json:"label"`
 	State string `json:"state"`
+	Size  int64  `json:"size"`
 }
 
 // Release represents a Github Release.
@@ -207,33 +207,109 @@ func CreateRelease(tag, branch, desc string, filepaths []string) {
 	publishRelease(release, filepaths)
 }
 
-func deleteAssetByFilename(releaseTag string, filename string) error {
-	data, _, err := doRequest("GET", fmt.Sprintf("%s/releases/tags/%s", githubAPIEndpoint, releaseTag), "application/json", nil, int64(0))
+type AssetHasCorrectSizeError struct {
+	Asset Asset
+}
+
+func (a AssetHasCorrectSizeError) Error() string {
+	return fmt.Sprintf("asset %s has expected file size of %d", a.Asset.Name, a.Asset.Size)
+}
+
+func deleteAssetWithWrongFileSize(releaseTag string, fileName string, fileSize int64) error {
+	asset, err := getAssetByFilename(releaseTag, fileName)
 	if err != nil {
 		return err
+	}
+
+	if asset.Size == fileSize {
+		return AssetHasCorrectSizeError{Asset: asset}
+	}
+
+	log.Printf("asset %s has incorrect file size of %d; deleting it.\n", asset.Name, asset.Size)
+	_, _, err = doRequest("DELETE", fmt.Sprintf("%s/releases/assets/%d", githubAPIEndpoint, asset.Id), "application/json", nil, int64(0))
+	return err
+}
+
+func getAssetByFilename(releaseTag string, filename string) (Asset, error) {
+	assetResult := Asset{}
+	data, _, err := doRequest("GET", fmt.Sprintf("%s/releases/tags/%s", githubAPIEndpoint, releaseTag), "application/json", nil, int64(0))
+	if err != nil {
+		return assetResult, err
 	}
 
 	var release Release
 	err = json.Unmarshal(data, &release)
 	if err != nil {
-		return err
+		return assetResult, err
 	}
 
-	var assertIndexToDelete = -1
+	assetIndex := -1
 	for i := range release.Assets {
 		if release.Assets[i].Name == filename {
-			assertIndexToDelete = i
+			assetIndex = i
 			break
 		}
 	}
 
-	if assertIndexToDelete == -1 {
-		return fmt.Errorf("could not find asset %s to delete", filename)
+	if assetIndex == -1 {
+		return assetResult, fmt.Errorf("could not find asset %s", filename)
 	}
 
-	_, _, err = doRequest("DELETE", fmt.Sprintf("%s/releases/assets/%d", githubAPIEndpoint, release.Assets[assertIndexToDelete].Id), "application/json", nil, int64(0))
+	assetResult = release.Assets[assetIndex]
 
-	return err
+	return assetResult, nil
+}
+
+func uploadFileWithRetry(uploadURL string, filePath string, releaseTag string, retryLimit int) error {
+
+	fileStat, err := os.Stat(filePath)
+	if err != nil {
+		return err
+	}
+
+	fileSize := fileStat.Size()
+	fileName := filepath.Base(filePath)
+	waitTime := time.Second
+	retryCount := 0
+
+	// The loop only breaks if we see that the asset exists and has the correct size
+	for {
+
+		log.Printf("Deleting asset %s if it exists and has incorrect file size (expecting %d).\n", fileName, fileSize)
+		err := deleteAssetWithWrongFileSize(releaseTag, fileName, fileSize)
+
+		if _, ok := err.(AssetHasCorrectSizeError); ok {
+			log.Printf("Asset %s exists and seems to have the correct file size of %d.\n", fileName, fileSize)
+			break
+		}
+
+		if err != nil {
+			log.Printf("Deleting asset failed, which is OK: %s\n", err.Error())
+		}
+
+		if retryCount >= retryLimit {
+			return fmt.Errorf("Retry limit of %d reached.\n", retryLimit)
+		}
+
+		if retryCount > 0 {
+			waitTime = waitTime * 2
+			log.Printf("Retrying upload after waiting %d second(s).\n", waitTime/time.Second)
+			time.Sleep(waitTime)
+		}
+
+		_, _, err = uploadFile(uploadURL, filePath)
+
+		if err != nil {
+			log.Printf("Failed to upload asset %s\n", filePath)
+			log.Printf("Error: %s\n", err.Error())
+		}
+
+		// Regardless of whether the upload gave an error, we loop until we confirm that the asset exists and has
+		// the correct size.
+		retryCount++
+	}
+
+	return nil
 }
 
 func publishRelease(release Release, filepaths []string) {
@@ -267,42 +343,11 @@ func publishRelease(release Release, filepaths []string) {
 	uploadURL := strings.Split(release.UploadURL, "{")[0]
 
 	const retryLimit = 5
-	for _, file := range filepaths {
-
-		retryCount := 0
-		for {
-			if retryCount >= retryLimit {
-				log.Fatalf("Retry limit of %d reached.\n", retryLimit)
-			}
-			_, status, err := uploadFile(uploadURL, file)
-
-			if err != nil {
-				log.Printf("Failed to upload asset %s\n", file)
-				log.Printf("Error: %s\n", err)
-
-				if status >= 500 && status < 600 {
-
-					log.Printf("Attempting to delete asset %s before retrying upload.\n", file)
-					err = deleteAssetByFilename(release.TagName, path.Base(file))
-					if err != nil {
-						log.Printf("Failed to delete asset. Ignoring error: %s.\n", err)
-					} else {
-						log.Printf("Successfully deleted %s\n", file)
-					}
-
-					time.Sleep(time.Duration(retryCount*retryCount) * time.Second)
-					log.Println("Retrying.")
-					retryCount++
-					continue
-				}
-
-				// If one file fails (and it is not a 5xx error) then this is a fatal error
-				os.Exit(1)
-			}
-
-			break
+	for _, filePath := range filepaths {
+		err := uploadFileWithRetry(uploadURL, filePath, release.TagName, retryLimit)
+		if err != nil {
+			log.Fatalln(err)
 		}
-
 	}
 }
 
